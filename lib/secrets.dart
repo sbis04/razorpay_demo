602002b321bc439d385a00315fd6221cda012117
Souvik Biswas <sbis1999@gmail.com>
2023-01-17 14:26:26 +0530

    Add secrets format

---
+++ lib/secrets.dart
@@ -0,0 +1,7 @@
+// You can generate your Razorpay Test/Live API Key
+// by logging in and generating one from:
+// Dashboard > Setting page (left menu) > API Keys tab > Generate Key
+class RazorpaySecret {
+  static const keyId = '<KEY_ID_HERE>';
+  static const keySecret = '<KEY_SECRET_HERE>';
+}
